47d96ab535b7d1b8c8dda441ec72a511ddbe5780
Nobuhiko Sawai <nobuhiko.sawai@gmail.com>
2019-11-02 01:23:58 -0700

    chore: add comment

--- answers/two-sum/index.ts
+++ answers/two-sum/index.ts
@@ -1,3 +1,6 @@
+// 1. Two Sum
+// https://github.com/nobuhikosawai/leetcode/pull/1
+
 export const twoSum = (nums: number[], target: number): number[] | undefined => {
   let res;
   let indexMap = new Map<number, number>();
