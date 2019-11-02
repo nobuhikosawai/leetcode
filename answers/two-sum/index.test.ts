89143e2ac7f2c08743ddb02a8644aeb884837085
nobuhikosawai <nobuhikosawai@users.noreply.github.com>
2019-11-02 10:37:59 -0700

    two-sum (#1)
    * feture: first two-sum
    
    * fix: update two-sum by removing over calculation
    
    * fix: update search by using hashmap
    
    * fix: use one-loop
    
    * chore: add comment

---
+++ answers/two-sum/index.test.ts
@@ -0,0 +1,31 @@
+import { twoSum } from './index';
+
+describe('twoSum', () => {
+  describe('normal', () => {
+    test('returns correct value', () => {
+      expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+    });
+
+    test('returns correct value', () => {
+      expect(twoSum([2, 7, 11, 15], 26)).toEqual([2, 3]);
+    });
+
+    test('returns correct value', () => {
+      expect(twoSum([2, 7, 11, 15], 22)).toEqual([1, 3]);
+    });
+
+    test('returns correct value', () => {
+      expect(twoSum([3, 3], 6)).toEqual([0, 1]);
+    });
+
+    test('returns correct value', () => {
+      expect(twoSum([1, 3, 4, 2], 6)).toEqual([2, 3]);
+    });
+  });
+
+  describe('when [incorrect target is given', () => {
+    test('returns undefined', () => {
+      expect(twoSum([2, 7, 11, 15], 3)).toEqual(undefined);
+    });
+  })
+});
---
+++ answers/two-sum/index.ts
@@ -0,0 +1,18 @@
+// 1. Two Sum
+// https://github.com/nobuhikosawai/leetcode/pull/1
+
+export const twoSum = (nums: number[], target: number): number[] | undefined => {
+  let res;
+  let indexMap = new Map<number, number>();
+
+  nums.some((n, i) => {
+    const targetNum = target - n;
+    const foundIndex = indexMap.get(targetNum);
+    if(foundIndex !== undefined && foundIndex !== i) {
+      res = [foundIndex, i];
+      return true
+    }
+    indexMap.set(n, i)
+  })
+  return res;
+}
