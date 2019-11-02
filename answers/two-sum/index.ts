308cfe613a8bc97206d77603ff98af5786cde9ff
Nobuhiko Sawai <nobuhiko.sawai@gmail.com>
2019-11-02 01:22:07 -0700

    fix: use one-loop

--- answers/two-sum/index.ts
+++ answers/two-sum/index.ts
@@ -2,18 +2,14 @@ export const twoSum = (nums: number[], target: number): number[] | undefined => 
   let res;
   let indexMap = new Map<number, number>();
 
-  // create indexMap
-  nums.forEach((n, i) => {
-    indexMap.set(n, i)
-  });
-
   nums.some((n, i) => {
     const targetNum = target - n;
     const foundIndex = indexMap.get(targetNum);
-    if(foundIndex && foundIndex !== i) {
-      res = [i, foundIndex];
+    if(foundIndex !== undefined && foundIndex !== i) {
+      res = [foundIndex, i];
       return true
     }
+    indexMap.set(n, i)
   })
   return res;
 }
