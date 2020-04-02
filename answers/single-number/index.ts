469c40ff70a4a881be723dd7a93a8365369d19b8
nobuhikosawai <nobuhikosawai@users.noreply.github.com>
2020-04-03 01:33:14 +0900

    single-number (#2)
    * feature: single-number
    
    * fix: use bit operator for single-number

---
+++ answers/single-number/index.ts
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/single-number/solution/
+
+export const singleNumber = (nums: number[]): number => {
+  let a = 0;
+
+  nums.forEach(n => {
+    a ^= n;
+  });
+
+  return a;
+};
