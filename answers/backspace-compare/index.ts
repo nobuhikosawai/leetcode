ff31a888b594446a6cac2823dbc608a0120cd2c2
nobuhikosawai <nobuhikosawai@users.noreply.github.com>
2020-04-10 01:59:13 +0900

    backspace-compare (#8)
    * feature: backspace-compare
    
    * fix: logic
    
    * chore: add comment

---
+++ answers/backspace-compare/index.ts
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/backspace-string-compare
+export const backspaceCompare = (S: string, T: string): boolean => {
+  let backSpacedS = '';
+  let backSpacedT = '';
+
+  for (let i = 0; i < S.length; i++) {
+    const char = S[i]
+    if (char === '#') {
+      backSpacedS = backSpacedS.slice(0, -1);
+    } else {
+      backSpacedS += char;
+    }
+  }
+
+  for (let i = 0; i < T.length; i++) {
+    const char = T[i]
+    if (char === '#') {
+      backSpacedT = backSpacedT.slice(0, -1);
+    } else {
+      backSpacedT += char;
+    }
+  }
+
+  return backSpacedS === backSpacedT;
+};
