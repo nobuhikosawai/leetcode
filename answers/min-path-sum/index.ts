f553230fd4df09c1024fa1f74cae81cb84189254
Nobuhiko Sawai <nobuhiko.sawai@gmail.com>
2020-04-20 00:37:51 +0900

    fix: performance (still slow)

--- answers/min-path-sum/index.ts
+++ answers/min-path-sum/index.ts
@@ -6,10 +6,12 @@ export const minPathSum = (grid: number[][]): number => {
 
   const dfs = (sum: number, i: number, j: number) => {
     const val = grid[i] && grid[i][j];
-    if (val === undefined) { return sum; }
+    if (val === undefined) { return; }
 
     sum += val;
     
+    if (minSum !== null && minSum < sum) { return; }
+
     if (i === numRow - 1 && j === numColumn - 1) {
       if (minSum !== null && minSum > sum) {
         minSum = sum;
