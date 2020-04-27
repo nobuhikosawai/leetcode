c2793dad616a424ec748001db03e1fd182b7da22
Nobuhiko Sawai <nobuhiko.sawai@gmail.com>
2020-04-27 15:08:44 +0900

    feature: implement longestCommonSubsequence

---
+++ answers/longest-common-subsequence/index.test.ts
@@ -0,0 +1,20 @@
+import { longestCommonSubsequence } from './index';
+
+describe('longestCommonSubsequence', () => {
+  test('ok', () => {
+    expect(longestCommonSubsequence("ab", "ac")).toEqual(1);
+  });
+
+  test('ok', () => {
+    expect(longestCommonSubsequence("abcde", "ace")).toEqual(3);
+  });
+
+  test('ok', () => {
+    expect(longestCommonSubsequence("abc", "abc")).toEqual(3);
+  });
+
+  test('ok', () => {
+    expect(longestCommonSubsequence("abc", "def")).toEqual(0);
+  });
+});
+
