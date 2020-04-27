0e8ed3c15b5e5b31eaab77e686ec60aa4b2e2f09
Nobuhiko Sawai <nobuhiko.sawai@gmail.com>
2020-04-27 15:16:43 +0900

    featue: use memoize

--- answers/longest-common-subsequence/index.ts
+++ answers/longest-common-subsequence/index.ts
@@ -1,23 +1,30 @@
 export const longestCommonSubsequence = (text1: string, text2: string): number => {
-  // todo: memoize
+  let memo: {[key: string]: number} = {};
   const dp = (i:number, j:number): number => {
+    const memoKey = [i, j].toString();
+    if (memo[memoKey] !== undefined) {
+      return memo[memoKey];
+    }
+
     if (i === 0 || j === 0) {
-      return 0;
+      memo[memoKey] = 0;
     } else if (i === 1 && j === 1) {
       const firstText = text1.slice(0, i);
       const secondText = text2.slice(0, j);
 
-      return firstText === secondText ? 1 : 0;
+      memo[memoKey] = firstText === secondText ? 1 : 0;
     } else {
       const lastChar1 = text1.slice(i - 1, i);
       const lastChar2 = text2.slice(j - 1, j);
 
       if (lastChar1 && lastChar2 && lastChar1 === lastChar2) {
-        return dp(i - 1, j - 1) + 1
+        memo[memoKey] = dp(i - 1, j - 1) + 1
       } else {
-        return Math.max(dp(i, j - 1), dp(i - 1, j));
+        memo[memoKey] = Math.max(dp(i, j - 1), dp(i - 1, j));
       }
     }
+
+    return memo[memoKey];
   }
 
   return dp(text1.length, text2.length);
