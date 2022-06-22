3796cc781d747711c9c7b7a2a1b42636cdf256bd
nobuhikosawai <nobuhiko.sawai@gmail.com>
2022-06-22 10:00:41 +0800

    chore: use while loop

--- answers/add-two-numbers/index.ts
+++ answers/add-two-numbers/index.ts
@@ -20,25 +20,32 @@ export class ListNode {
 }
 
 export function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-  const getValue = (l1: ListNode | null, l2: ListNode | null, carryover: number): ListNode => {
-    if (!l1 && !l2 && !carryover) {
-      return null;
-    }
+  let nextL1 = l1, nextL2 = l2, carryover = 0, headNode, prevNode;
 
-    const sum = (l1?.val ?? 0) + (l2?.val ?? 0) + carryover;
+  while (nextL1 || nextL2 || carryover) {
+    const sum = (nextL1?.val ?? 0) + (nextL2?.val ?? 0) + carryover;
 
     let val: number
-    let nextCarryover: number
     if (sum >= 10) {
       val = sum % 10;
-      nextCarryover = 1;
+      carryover = 1;
     } else {
       val = sum;
-      nextCarryover = 0;
+      carryover = 0;
     }
 
-    return new ListNode(val, getValue(l1?.next, l2?.next, nextCarryover));
+    const node = new ListNode(val, null)
+
+    if (prevNode) {
+      prevNode.next = node
+    } else {
+      headNode = node
+    }
+
+    prevNode = node
+    nextL1 = nextL1?.next
+    nextL2 = nextL2?.next
   }
 
-  return getValue(l1, l2, 0);
+  return headNode;
 };
