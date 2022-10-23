f7ac097da98a72da33fb2323b359096b38ed747a
nobuhikosawai <nobuhiko.sawai@gmail.com>
2022-10-23 22:14:06 +0900

    fix: comment

--- answers/best-time-to-buy-and-sell-stock/index.ts
+++ answers/best-time-to-buy-and-sell-stock/index.ts
@@ -1,3 +1,5 @@
+// 121. Best Time to Buy and Sell Stock
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 export function maxProfit(prices: number[]): number {
   let profit = 0;
   let minPrice = +Infinity;
