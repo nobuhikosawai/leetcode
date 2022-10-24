b2d34737799b34b18450121b39e72b58fa2a043e
nobuhikosawai <nobuhiko.sawai@gmail.com>
2022-10-24 10:24:52 +0900

    2nd attempt

--- answers/climbing-stairs/index.ts
+++ answers/climbing-stairs/index.ts
@@ -1,17 +1,17 @@
 // 70. Climbing Stairs
 // https://leetcode.com/problems/climbing-stairs/
 export function climbStairs(n: number): number {
-  const results = Array(n).fill(0);
-  results[0] = 1;
-  results[1] = 2;
-  if (n < 3) {
-    return results[n - 1];
+  if (n === 1) {
+    return 1;
   }
+  let first = 1;
+  let second = 2;
 
   for (let i = 2; i < n; i++) {
-    const res = results[i - 1] + results[i - 2];
-    results[i] = res;
+    const res = first + second;
+    first = second;
+    second = res;
   }
 
-  return results[n - 1];
+  return second;
 }
