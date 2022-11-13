d6a39cdf5381df68311b210fae40315d3a041f12
nobuhikosawai <nobuhiko.sawai@gmail.com>
2022-11-13 17:04:38 +0900

    1st successful attempt

--- answers/combination-sum/index.ts
+++ answers/combination-sum/index.ts
@@ -4,53 +4,30 @@ export function combinationSum(
   candidates: number[],
   target: number
 ): number[][] {
-  const tmpRes = new Map<number, number[][]>();
+  function backtrack(
+    target: number,
+    results: number[][],
+    combination: number[],
+    startIndex: number
+  ) {
+    if (target === 0) {
+      results.push([...combination]);
+      return;
+    }
 
-  function helper(candidates: number[], target: number): void {
-    candidates.forEach((c, i) => {
-      const next = target - c;
+    if (target < 0) {
+      return;
+    }
 
-      if (next < 0) {
-        return;
-      }
-
-      if (next === 0) {
-        tmpRes.has(target)
-          ? tmpRes.get(target).push([c])
-          : tmpRes.set(target, [[c]]);
-        return;
-      }
-
-      helper(candidates, next);
-
-      if (tmpRes.has(next)) {
-        const res = tmpRes.get(next);
-
-        const newRes = res.map((r) => {
-          r.push(c);
-          return r;
-        });
-
-        console.log('res', res);
-        console.log('newres', newRes);
-
-        if (tmpRes.has(target)) {
-          const tRes = tmpRes.get(target);
-
-          tRes.push(...newRes);
-        } else {
-          tmpRes.set(target, newRes);
-        }
-      }
-    });
+    for (let i = startIndex; i < candidates.length; i++) {
+      const candidate = candidates[i];
+      combination.push(candidate);
+      backtrack(target - candidate, results, combination, i);
+      combination.pop();
+    }
   }
 
-  helper(candidates, target);
-
-  // TODO: consider unique
-  const res: number[][] = [];
-  for (const v of tmpRes.values()) {
-    res.push(...v);
-  }
-  return res;
+  const results: number[][] = [];
+  backtrack(target, results, [], 0);
+  return results;
 }
