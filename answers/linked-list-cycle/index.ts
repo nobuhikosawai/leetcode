b26215c3c8942884ec766ac61aef323aa9d227e4
nobuhikosawai <nobuhiko.sawai@gmail.com>
2022-11-28 04:00:02 +0900

    2nd

--- answers/linked-list-cycle/index.ts
+++ answers/linked-list-cycle/index.ts
@@ -3,24 +3,13 @@ import { ListNode } from '../../data-structures/list-node';
 // 141. Linked List Cycle
 // https://leetcode.com/problems/linked-list-cycle/description/
 export function hasCycle(head: ListNode | null): boolean {
-  let visited: Record<number, ListNode[]> = {};
-
-  function helper(head: ListNode | null): boolean {
-    if (head === null) {
-      return false;
-    }
-    const candidates = visited[head.val];
-    if (candidates?.length > 0 && candidates.includes(head)) {
+  const set = new Set<ListNode>();
+  while (head) {
+    if (set.has(head)) {
       return true;
     }
-
-    if (!candidates) {
-      visited[head.val] = [head];
-    } else {
-      candidates.push(head);
-    }
-    return helper(head.next);
+    set.add(head);
+    head = head.next;
   }
-
-  return helper(head);
+  return false;
 }
