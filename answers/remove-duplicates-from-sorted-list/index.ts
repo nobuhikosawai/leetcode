89e406f6e09131e57078a219dd9f6206c5bd47af
nobuhikosawai <nobuhiko.sawai@gmail.com>
2022-11-29 01:48:19 +0900

    while version

--- answers/remove-duplicates-from-sorted-list/index.ts
+++ answers/remove-duplicates-from-sorted-list/index.ts
@@ -3,15 +3,14 @@ import { ListNode } from '../../data-structures/list-node';
 // 83. Remove Duplicates from Sorted List
 // https://leetcode.com/problems/remove-duplicates-from-sorted-list/
 export function deleteDuplicates(head: ListNode | null): ListNode | null {
-  if (!head) {
-    return null;
-  }
+  let current = head;
 
-  if (head.next && head.val === head.next.val) {
-    head.next = head.next.next;
-    deleteDuplicates(head);
-  } else {
-    deleteDuplicates(head.next);
+  while (current) {
+    if (current.next && current.val === current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
   }
 
   return head;
