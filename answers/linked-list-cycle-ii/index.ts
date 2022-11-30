dbcbbece2c16270d156f3f458f0e0bf71757608d
nobuhikosawai <nobuhiko.sawai@gmail.com>
2022-12-01 04:25:55 +0900

    another solution

--- answers/linked-list-cycle-ii/index.ts
+++ answers/linked-list-cycle-ii/index.ts
@@ -3,15 +3,40 @@ import { ListNode } from '../../data-structures/list-node';
 // 142. Linked List Cycle II
 // https://leetcode.com/problems/linked-list-cycle-ii/
 export function detectCycle(head: ListNode | null): ListNode | null {
-  const visited = new Set<ListNode>();
+  // const visited = new Set<ListNode>();
+  //
+  // while (head) {
+  //   if (visited.has(head)) {
+  //     return head;
+  //   }
+  //   visited.add(head);
+  //   head = head.next;
+  // }
+  //
+  // return null;
 
-  while (head) {
-    if (visited.has(head)) {
-      return head;
+  let slow = head;
+  let fast = head;
+  let intr = null;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      intr = slow;
+      break;
     }
-    visited.add(head);
-    head = head.next;
   }
 
-  return null;
+  if (!intr) {
+    return null;
+  }
+
+  while (head !== intr) {
+    head = head.next;
+    intr = intr.next;
+  }
+
+  return head;
 }
