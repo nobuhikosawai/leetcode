17f15510609c64e909a4e3525d71ef202638162e
nobuhikosawai <nobuhiko.sawai@gmail.com>
2022-12-01 23:18:40 +0900

    use sentinel

--- answers/remove-duplicates-from-sorted-list-ii/index.ts
+++ answers/remove-duplicates-from-sorted-list-ii/index.ts
@@ -3,30 +3,22 @@ import { ListNode } from '../../data-structures/list-node';
 // 82. Remove Duplicates from Sorted List II
 // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description/
 export function deleteDuplicates(head: ListNode | null): ListNode | null {
-  let prev = null;
-  let dup = null;
-  let current = head;
+  let sentinel = new ListNode(0, head);
 
-  while (current) {
-    if (current.val === dup) {
-      if (prev) {
-        prev.next = current.next;
-      } else {
+  let pred = sentinel;
+
+  while (head) {
+    if (head.next && head.val === head.next.val) {
+      while (head.next && head.val === head.next.val) {
         head = head.next;
       }
-      current = current.next;
-    } else if (current.val === current.next?.val) {
-      dup = current.val;
-      if (prev) {
-        prev.next = current.next.next;
-      } else {
-        head = head.next.next;
-      }
-      current = current.next.next;
+      pred.next = head.next;
     } else {
-      prev = current;
-      current = current.next;
+      pred = pred.next;
     }
+
+    head = head.next;
   }
-  return head;
+
+  return sentinel.next;
 }
